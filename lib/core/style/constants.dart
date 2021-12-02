406cde07550ccb3c70fc1110a797e607e5cc3f69
mkprogrammer15 <michael@neon.dev>
2021-12-02 12:36:11 +0100

    updated folder structure

---
+++ lib/core/style/constants.dart
@@ -0,0 +1,30 @@
+// COLOR CONSTANTS ------------------------------------------------------------
+
+import 'package:flutter/material.dart';
+
+const kColorPrimary = Color(0xff100084);
+const kColorAccent = Color(0xffD4CEFF);
+const kColorWhite = Color(0xffFFFFFF);
+const kColorPink = Color(0xffCF2D71);
+const kColorLila = Color(0xffAA00EA);
+const kColorDarkBlue = Color(0xff4D72DE);
+const kColorOrange = Color(0xffE28919);
+const kColorGreen = Color(0xff29B174);
+
+const kColorBackground = Color(0xffF2F2F2);
+const kColorBlack = Color(0xff222B45);
+const kColorGreyInput = Color(0xffFBFBFB);
+final kColorGreyBorder = const Color(0xff979797).withOpacity(0.142);
+const kColorHint = Color(0xff777777);
+const kColorDivider = Color(0xff979797);
+const kColorBlue = Color(0xff00A8EA);
+const kColorDanger = Color(0xffEE5953);
+const kColorDarkDanger = Color(0xffB60000);
+const kColorWarning = Color(0xffEABD00);
+const kColorDarkWarning = Color(0xffEA8A00);
+
+const kColorSuccess = Color(0xff00AD19);
+const kColorEnabledBorder = Color(0xffF3F2F2);
+final kColorDeactiveButton = const Color(0xff100084).withOpacity(0.1);
+const kColorTopupGreen = Color(0xff39AE65);
+const kColorTopupRed = Color(0xffDC5B5B);
