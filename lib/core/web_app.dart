38576186417218c895661818fd2c9350bd91782c
MaximilianKliche <maximiliankliche4@gmail.com>
2021-12-14 16:47:39 +0100

    Adding Injectable and Geit for instanciation

--- lib/core/web_app.dart
+++ lib/core/web_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neon_web/features/overview/presentation/blocs/filter_bloc.dart';
 import 'package:neon_web/features/overview/presentation/pages/overview_page.dart';
+import '../injection_container.dart';
 
 class WebApp extends StatelessWidget {
   @override
@@ -9,8 +10,8 @@ class WebApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<FilterBloc>(
-            create: (context) =>
-                FilterBloc()..add(const FilterEvent.filterMenuEventType())),
+            create: (context) => getIt<FilterBloc>()
+              ..add(const FilterEvent.filterMenuEventType()))
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
---
+++ lib/injection_container.dart
@@ -0,0 +1,8 @@
+import 'injection_container.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;
+
+@injectableInit
+void configureDependencies() => $initGetIt(getIt);
