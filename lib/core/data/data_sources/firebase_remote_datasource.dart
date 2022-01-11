0a9ada94d36490cd052b445d5a81cedc98276752
Maximilian Kliche <maximiliankliche@MBPvonMximilian.fritz.box>
2022-01-11 16:27:08 +0100

    update

--- lib/core/data/data_sources/firebase_remote_datasource.dart
+++ lib/core/data/data_sources/firebase_remote_datasource.dart
@@ -3,10 +3,12 @@ import 'package:injectable/injectable.dart';
 import 'package:neon_web/core/error/failure.dart';
 import 'package:neon_web/core/success/success.dart';
 import 'package:dartz/dartz.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 @injectable
 class FireBaseRemoteDataSource {
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final firestoreInstance = FirebaseFirestore.instance;
 
   Future<Either<Failure, Success>> firebaseLogin(
       {required String email, required String password}) async {
@@ -14,10 +16,24 @@ class FireBaseRemoteDataSource {
       final result = await firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
 
-      return result.user == null ? Left(FireBaseFailure()) : Right(FireBaseSuccess());
+      return result.user == null
+          ? Left(FireBaseFailure())
+          : Right(FireBaseSuccess());
     } on FirebaseAuthException catch (error) {
       print(error.message);
       return Left(FireBaseFailure());
     }
   }
+
+ // Future<Either<Failure, Success>> uploadProject() async {
+//
+//
+//
+ //   try {
+//
+ //     
+ //   } on FirebaseException catch (error) {
+ //     print(error);
+ //   }
+ // }
 }
